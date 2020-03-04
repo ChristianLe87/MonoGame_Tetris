9fba53043bb8f37e5a3a36eaa620e4953972b92c
Christian <christianle87@icloud.com>
2020-03-04 01:12:04 +0100

    add grid class

--- Shared/Assets/GameGrid.cs
+++ Shared/Assets/GameGrid.cs
@@ -6,46 +6,83 @@ namespace Shared
 {
     public class GameGrid
     {
-        char[,] grid;
+        public char[,] grid = new char[,] {
+                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
+                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
+                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
+                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
+                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
+                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
+                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
+                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
+                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
+                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
+                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
+                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
+                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
+                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
+                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
+                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
+                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
+                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
+                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
+                                    { '|', '-', '-', '-', '-', '-', '-', '-', '-', '|' },
+                                    };
 
-        Texture2D backgrownd;
-        Rectangle rectangle;
+        public Texture2D textureBackgrownd;
+        public Texture2D texturePlayer;
+        public Texture2D textureBorder;
 
-        public GameGrid(Rectangle rectangle, int unitsWidth, int unitsHeight)
+
+        public GameGrid()
         {
 
-
-            backgrownd = Tools.CreateColorTexture(Color.LightBlue);
-
-            this.rectangle = rectangle;
-            InitialiseGrid(unitsHeight, unitsWidth);
+            textureBackgrownd = Tools.CreateColorTexture(Color.Pink);
+            texturePlayer = Tools.CreateColorTexture(Color.Red);
+            textureBorder = Tools.CreateColorTexture(Color.DarkGreen);
         }
 
 
-
-        public void Update()
+        public void Update(Piece_S piece_S)
         {
+            // burn grid
+            {
 
+                if (piece_S.canDown == false)
+                {
+                    this.grid = Tools.BurnPieceIntoGrid(this.grid, piece_S.pieceDesign, piece_S.playerPosition);
+                    piece_S.playerPosition = new Vector2(1, 0);
+                    this.grid = Tools.DeliteLine(this.grid);
+                }
+            }            
         }
-
 
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            spriteBatch.Draw(backgrownd, rectangle, Color.White);
-        }
-
-        public void InitialiseGrid(int unitsHeight, int unitsWidth)
-        {
-            this.grid = new char[unitsWidth, unitsHeight];
-
-            for (int row = 0; row < unitsHeight; row++)
+            // Draw grid
             {
-                for (int col = 0; col < unitsWidth; col++)
+                for (int row = 0; row < 10; row++)
                 {
-                    grid[col, row] = ' ';
+                    for (int col = 0; col < 20; col++)
+                    {
+                        switch (this.grid[col, row])
+                        {
+                            case ' ':
+                                spriteBatch.Draw(this.textureBackgrownd, new Rectangle(row * 10, col * 10, 10, 10), Color.White);
+                                break;
+                            case 'x':
+                                spriteBatch.Draw(this.texturePlayer, new Rectangle(row * 10, col * 10, 10, 10), Color.White);
+                                break;
+                            default:
+                                spriteBatch.Draw(this.textureBorder, new Rectangle(row * 10, col * 10, 10, 10), Color.White);
+                                break;
+                        }
+                    }
                 }
             }
         }
+
+       
     }
 }
--- Shared/Scenes/Escena_1.cs
+++ Shared/Scenes/Escena_1.cs
@@ -10,98 +10,27 @@ namespace Shared
     public class Escena_1 : IScene
     {
 
-        char[,] grid = new char[,] {
-                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
-                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
-                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
-                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
-                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
-                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
-                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
-                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
-                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
-                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
-                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
-                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
-                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
-                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
-                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
-                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
-                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
-                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
-                                    { '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '|' },
-                                    { '|', '-', '-', '-', '-', '-', '-', '-', '-', '|' },
-                                    };
-
-        Texture2D textureBackgrownd;
-        Texture2D texturePlayer;
-        Texture2D textureBorder;
-
-
-
-
-
-
-
+        GameGrid gameGrid;
         Piece_S piece_S;
 
         public Escena_1()
         {
             piece_S = new Piece_S(new Vector2(1, 0));
 
+            gameGrid = new GameGrid();
 
-            textureBackgrownd = Tools.CreateColorTexture(Color.Pink);
-            texturePlayer = Tools.CreateColorTexture(Color.Red);
-            textureBorder = Tools.CreateColorTexture(Color.DarkGreen);
         }
 
         public void Update()
         {
-            piece_S.Update(grid);
-
-
-           
-
-
-
-
-            // burn grid
-            {
-
-                if (piece_S.canDown == false)
-                {
-                    grid = Tools.BurnPieceIntoGrid(grid, piece_S.pieceDesign, piece_S.playerPosition);
-                    piece_S.playerPosition = new Vector2(1, 0);
-                    grid = Tools.DeliteLine(grid);
-                }
-            }
-
-
+            piece_S.Update(gameGrid.grid);
+            gameGrid.Update(piece_S);
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            // Draw grid
-            {
-                for (int row = 0; row < 10; row++)
-                {
-                    for (int col = 0; col < 20; col++)
-                    {
-                        switch (grid[col, row])
-                        {
-                            case ' ':
-                                spriteBatch.Draw(textureBackgrownd, new Rectangle(row * 10, col * 10, 10, 10), Color.White);
-                                break;
-                            case 'x':
-                                spriteBatch.Draw(texturePlayer, new Rectangle(row * 10, col * 10, 10, 10), Color.White);
-                                break;
-                            default:
-                                spriteBatch.Draw(textureBorder, new Rectangle(row * 10, col * 10, 10, 10), Color.White);
-                                break;
-                        }
-                    }
-                }
-            }
+            gameGrid.Draw(spriteBatch);
+           
 
 
             piece_S.Draw(spriteBatch);
